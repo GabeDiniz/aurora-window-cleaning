19ad4ab5ec868000d7e1b78507a0ed82a7e24ebb
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-04-12 17:33:06 -0400

    [Init] reinit project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
