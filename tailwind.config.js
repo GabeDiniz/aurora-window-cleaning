4f0dbb4ca4de2d8fce382de15ba80380bc7d11e9
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-04-13 00:29:52 -0400

    [Setup] configure tailwind and tsconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        classicBlue: "#013A81",
+      },
+    },
   },
   plugins: [],
 };
