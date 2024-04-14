41f9042091b8c17dbd9617601fd216cb1feef7e8
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-04-13 22:50:48 -0400

    [Resolve] routing and config issues

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,9 @@
 const Landing: React.FC = () => {
-  return <div className="pt-[5rem] transition ease-in"></div>;
+  return (
+    <div className="pt-[5rem] transition ease-in">
+      <h1>Testing</h1>
+    </div>
+  );
 };
 
 export { Landing };
