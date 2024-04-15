da9dd25b5c135cb0c6d103c1992c9915b11f936a
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-04-14 20:10:55 -0400

    [Update] Navbar and hero

--- src/components/Hero.section.tsx
+++ src/components/Hero.section.tsx
@@ -2,9 +2,10 @@ const HeroSection: React.FC = () => {
   return (
     <section className="w-screen h-screen flex justify-center items-center bg-slate-400">
       {/* img here */}
-      <div className="flex flex-col items-center">
-        <h2>Serving Aurora</h2>
-        <h1>StudentWorks Cleaning</h1>
+      <div className="flex flex-col items-center gap-5 text-white">
+        <h2 className="text-3xl uppercase">Serving Aurora</h2>
+        <h1 className="text-7xl uppercase">StudentWorks Cleaning</h1>
+        <button>Contact Us</button>
       </div>
     </section>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,8 @@
 const Navbar: React.FC = () => {
   return (
-    <section className="fixed w-full flex justify-between px-10 py-5 bg-classicBlue text-white">
-      <h1 className="uppercase">Logo here</h1>
-      <div className="uppercase">
+    <section className="fixed w-full flex justify-between px-10 py-5 text-white">
+      <h1 className="">StudentWorks</h1>
+      <div className="uppercase flex gap-10">
         <a>Page 1</a>
         <a>Page 2</a>
         <a>Page 3</a>
