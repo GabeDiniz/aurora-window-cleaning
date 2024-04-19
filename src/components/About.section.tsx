6614cf57dccb0fab42b41c458635e013fb7814dd
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-04-18 22:16:08 -0400

    [Style] adjust padding for About section

--- src/components/About.section.tsx
+++ src/components/About.section.tsx
@@ -20,7 +20,7 @@ const AboutSection: React.FC = () => {
   return (
     <section className="py-20 flex flex-col justify-center items-center ]">
       <h1 className="text-4xl text-white">Why choose us</h1>
-      <div className="flex mt-10 text-white max-w-7xl gap-10">
+      <div className="flex mt-10 text-white max-w-7xl gap-10 px-20">
         {chooseUs.map((content, i) => (
           <div className="flex flex-col gap-5 text-center w-1/3" key={i}>
             <h2 className="text-xl">{content.title}</h2>
