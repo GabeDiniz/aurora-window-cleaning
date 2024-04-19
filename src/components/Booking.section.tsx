df43ac0fe2473448838ed52e1aa6f30bd1d540de
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-04-18 22:07:11 -0400

    [Styling] booking section

--- src/components/Booking.section.tsx
+++ src/components/Booking.section.tsx
@@ -2,8 +2,8 @@ import { HeroImage } from "@assets";
 
 const BookingSection: React.FC = () => {
   return (
-    <section className="flex justify-center gap-20">
-      <div className="flex flex-col text-white gap-4 mt-5">
+    <section className="mt-5 mb-16 flex justify-center gap-20 px-20">
+      <div className="flex flex-col text-white gap-4">
         <h1 className="text-4xl">Schedule your free estimate today!</h1>
         <p className="text-md">
           Schedule an estimate at your convenience. We look forward to meeting
@@ -11,7 +11,7 @@ const BookingSection: React.FC = () => {
         </p>
         <button className="max-w-64">Book estimate</button>
       </div>
-      <img className="w-1/4 object-cover rounded-lg" src={HeroImage} />
+      <img className="w-1/3 lg:w-1/4 object-cover rounded-lg" src={HeroImage} />
     </section>
   );
 };
