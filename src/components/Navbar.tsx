5701691fc2c0851a0490ee5292f1f49fde575d60
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-04-21 18:00:33 -0400

    [Setup] Navbar routing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Navbar: React.FC = () => {
   return (
     <section className="fixed px-10 py-5 z-10 w-full flex justify-between text-lg text-white">
-      <a href="" className="">
+      <Link to="/" className="">
         StudentWorks
-      </a>
+      </Link>
       <div className="uppercase flex gap-10 font-thin">
-        <a>Our Team</a>
-        <a>Services</a>
-        <a>Reviews</a>
-        <a>Free Estimate</a>
-        <a>Job Opportunities</a>
+        <Link to="/our-team">Our Team</Link>
+        <Link to="/">Services</Link>
+        <Link to="/">Reviews</Link>
+        <Link to="/">Free Estimate</Link>
+        <Link to="/">Job Opportunities</Link>
       </div>
     </section>
   );
