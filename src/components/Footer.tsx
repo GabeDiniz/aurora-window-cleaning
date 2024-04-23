093e9bfa1751d69fd21805eceefb91ed1cc14794
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-04-23 19:17:46 -0400

    [Small fix] Add footer tag to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 const Footer: React.FC = () => {
   return (
-    <section className="py-20 flex flex-col justify-center bg-white">
+    <footer className="py-20 flex flex-col justify-center bg-white">
       <h1 className="text-4xl text-center">Socials Here.</h1>
       <p className="uppercase text-xl text-center">Get in touch</p>
-    </section>
+    </footer>
   );
 };
 
