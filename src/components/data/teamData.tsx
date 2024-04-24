00f92a3861b6a5433f544cf38ee7b57ca750bc03
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-04-23 21:36:34 -0400

    [Add] Team data

---
+++ src/components/data/teamData.tsx
@@ -0,0 +1,21 @@
+const teamMembers = [
+  {
+    name: "Eric Faro",
+    title: "Owner",
+    description:
+      "Hi, my name is, what? My name is, who? My name is, chka-chka, Slim Shady Hi, my name is, huh? My name is, what? My name is, chka-chka, Slim Shady",
+  },
+  {
+    name: "Cole Benoit",
+    title: "Operator",
+    description:
+      "Hi, my name is, what? My name is, who? My name is, chka-chka, Slim Shady Hi, my name is, huh? My name is, what? My name is, chka-chka, Slim Shady",
+  },
+  {
+    name: "Thomas Edison",
+    title: "Marketer",
+    description:
+      "Hi, my name is, what? My name is, who? My name is, chka-chka, Slim Shady Hi, my name is, huh? My name is, what? My name is, chka-chka, Slim Shady",
+  },
+];
+export { teamMembers };
