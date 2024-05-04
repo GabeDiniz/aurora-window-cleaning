b56e12cffa612581f31d5252d815fed215e2180e
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-05-04 12:21:33 -0400

    [Update] Services styling

--- src/components/sections/services/Services.section.tsx
+++ src/components/sections/services/Services.section.tsx
@@ -22,21 +22,25 @@ import { servicesData } from "@data";
 
 const Services: React.FC = () => {
   return (
-    <section className="relative w-screen">
-      <div className="my-10">
+    <section className="relative w-screen bg-laceWhite">
+      <div className="py-10">
+        {/* MAP ALL SERVICES */}
         {servicesData.map((product) => {
           return (
-            <div className="text-3xl flex flex-col justify-center items-center">
+            <div className="text-3xl flex flex-col gap-5 mb-10 justify-center items-center">
               <h1>{product.title}</h1>
-              {product.data.map((item) => {
-                return (
-                  <div className="flex flex-col justify-center items-center">
-                    <img src={item.banner} className="w-1/3 rounded-md" />
-                    <h2 className="text-base">{item.subtitle}</h2>
-                    <p className="text-sm">{item.subtext}</p>
-                  </div>
-                );
-              })}
+              <div className="flex gap-5 mx-10">
+                {/* MAP EACH SERVICE SECTION DATA */}
+                {product.data.map((item) => {
+                  return (
+                    <div className="flex flex-col justify-center items-center gap-3">
+                      <img src={item.banner} className="w-full rounded-md" />
+                      <h2 className="text-base">{item.subtitle}</h2>
+                      <p className="text-sm">{item.subtext}</p>
+                    </div>
+                  );
+                })}
+              </div>
             </div>
           );
         })}
