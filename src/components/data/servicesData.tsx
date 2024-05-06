44760d773f0697db9c98c64ece764e786b78d2d6
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-05-06 18:13:10 -0400

    [Add] OurServices data

--- src/components/data/servicesData.tsx
+++ src/components/data/servicesData.tsx
@@ -14,13 +14,13 @@ const servicesData = [
         banner: HeroImage,
         subtitle: "Interior cleaning",
         subtext:
-          "All of our window cleaning is done by hand. Exterior cleaning includes the complete cleaning of all window panes, frames, and sills. ",
+          "Interior window cleaning includes cleaning of the window panes, frames, sills, screens, tracts, and opening mechanisms. ",
       },
       {
         banner: HeroImage,
         subtitle: "Rain-X",
         subtext:
-          "All of our window cleaning is done by hand. Exterior cleaning includes the complete cleaning of all window panes, frames, and sills. ",
+          "This product helps keep your windows clean longer. it is a water-repellant spray that can be applied after your windows have been thoroughly cleaned and dried. ",
       },
     ],
   },
@@ -29,9 +29,21 @@ const servicesData = [
     data: [
       {
         banner: HeroImage,
-        subtitle: "Exterior cleaning",
+        subtitle: "Interior Eavestrough Cleaning",
         subtext:
-          "All of our window cleaning is done by hand. Exterior cleaning includes the complete cleaning of all window panes, frames, and sills. ",
+          "This service removes all of the lodged debris from the eaves and downspouts to increase water flow and avoid overflowing. Interior eaves cleaning also stops water from freezing in the eaves, which can lead to cracked getters that need replacing. ",
+      },
+      {
+        banner: HeroImage,
+        subtitle: "Exterior Eavestrough Cleaning",
+        subtext:
+          "Exterior eaves cleaning includes thorough hand cleaning of the exterior eaves and/or soffits. This removes built-up dirt, mildew, and mold, which can cause permanent staining. It also makes a huge difference to the appearance of your home. ",
+      },
+      {
+        banner: HeroImage,
+        subtitle: "Gutter Guard Installation",
+        subtext:
+          "Gutter guards are a long-term solution for clean eavestroughs. These high-quality aluminum gutter guards clip on without the use of screws. They do not rust or crack, and they have a 25-year manufacturer warranty. We install two different brands; Gutter Ware, and Easy On.",
       },
     ],
   },
@@ -40,9 +52,27 @@ const servicesData = [
     data: [
       {
         banner: HeroImage,
-        subtitle: "Exterior cleaning",
+        subtitle: "Deck & Fence Pressure Washing",
         subtext:
-          "All of our window cleaning is done by hand. Exterior cleaning includes the complete cleaning of all window panes, frames, and sills. ",
+          "Pressure washing wood removes embedded dirt and mold. It is important to be trained on how to correctly pressure wash wood, as an incorrect technique can ruin a deck or fence. ",
+      },
+      {
+        banner: HeroImage,
+        subtitle: "Deck & Fence Handwashing",
+        subtext:
+          "Handwashing provides a deeper clean than standard pressure washing and gets harder-to-reach areas. A brightening solution is had scrubbed into the wood to remove embedded dirt. The wood is pressure washed to remove the brightener. ",
+      },
+      {
+        banner: HeroImage,
+        subtitle: "Stone & Brick Pressure Washing",
+        subtext:
+          "Dirt, moss, and mildew can build up on the surfaces around your home. This occurs after snow and water sit on your walkways/patios all winter. Stone pressure washing brightens and cleans up the exterior of your house. ",
+      },
+      {
+        banner: HeroImage,
+        subtitle: "Siding Pressure Washing",
+        subtext:
+          "Siding collects moss and dirt over time, which can stain the siding if it is not removed. Pressure washing removes the build-up of debris and cleans up the exterior of your home. ",
       },
     ],
   },
@@ -51,9 +81,15 @@ const servicesData = [
     data: [
       {
         banner: HeroImage,
-        subtitle: "Exterior cleaning",
+        subtitle: "Polymeric Sand Installation",
         subtext:
-          "All of our window cleaning is done by hand. Exterior cleaning includes the complete cleaning of all window panes, frames, and sills. ",
+          "We pressure wash the interlock/ stone to remove all of the dirt and mold from the surface and the joints. Then we fill the joints with polymeric sand. Getting your interlock or pavers re-sanded is very important. The sand helps keep the stones from falling and sinking into the ground and helps keep your driveway, path, or patio level. It also restores the patio's look and drastically changes your home's look! ",
+      },
+      {
+        banner: HeroImage,
+        subtitle: "Sealing",
+        subtext:
+          "Sealing your patio, driveway, or walkway is very important. It helps keep the stones clean and lengthens the time needed between re-sanding or pressure washing. It protects against oil, dirt, salt, anti-freeze, and transmission fluid. Sealing is a great solution whether you want to protect your patio or simply change the look. The products we use are water, based, and low odor. ",
       },
     ],
   },
