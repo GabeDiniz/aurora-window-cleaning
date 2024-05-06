2e1ba3ad7d3e1c9cb17a0a132847af43188eecbe
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-05-06 18:18:22 -0400

    [Update] OurServices and global styling

--- src/components/sections/services/Services.section.tsx
+++ src/components/sections/services/Services.section.tsx
@@ -1,25 +1,6 @@
 // import { HeroImage } from "@assets";
 import { servicesData } from "@data";
 
-// Mock Data
-// {
-//   title: "Window Cleaning",
-//   data: [
-//     {
-//       image: HeroImage,
-//       subtitle: "Exterior cleaning",
-//       subtext:
-//         "All of our window cleaning is done by hand. Exterior cleaning includes the complete cleaning of all window panes, frames, and sills. ",
-//     },
-//      {
-//       image: HeroImage,
-//       subtitle: "Exterior cleaning",
-//       subtext:
-//         "All of our window cleaning is done by hand. Exterior cleaning includes the complete cleaning of all window panes, frames, and sills. ",
-//     },
-//   ],
-// },
-
 const Services: React.FC = () => {
   return (
     <section className="relative w-screen bg-laceWhite">
@@ -29,14 +10,14 @@ const Services: React.FC = () => {
           return (
             <div className="text-3xl flex flex-col gap-5 mb-10 justify-center items-center">
               <h1>{product.title}</h1>
-              <div className="flex gap-5 mx-10">
+              <div className="flex gap-5 mx-10 items-start">
                 {/* MAP EACH SERVICE SECTION DATA */}
                 {product.data.map((item) => {
                   return (
-                    <div className="flex flex-col justify-center items-center gap-3">
+                    <div className="flex flex-col justify-center gap-3">
                       <img src={item.banner} className="w-full rounded-md" />
                       <h2 className="text-base">{item.subtitle}</h2>
-                      <p className="text-sm">{item.subtext}</p>
+                      <p className="text-sm font-thin">{item.subtext}</p>
                     </div>
                   );
                 })}
