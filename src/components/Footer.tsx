9ce9f3bd509ae5e3a31a61888e7f23692681b31a
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-05-09 20:41:42 -0400

    [Update] footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,18 @@
+import { AiFillFacebook } from "react-icons/ai";
+
 const Footer: React.FC = () => {
   return (
-    <footer className="py-20 flex flex-col justify-center bg-slate-900 text-laceWhite">
-      <h1 className="text-4xl text-center">Socials Here.</h1>
-      <p className="uppercase text-xl text-center">Get in touch</p>
+    <footer className="py-10 pl-10 flex flex-col gap-2 justify-center bg-slate-900 text-laceWhite">
+      <h1 className="text-4xl ">Get in touch</h1>
+      <p className="text-base ">efaro@hotmail.com</p>
+      <a
+        href="https://www.facebook.com/share/sHBNDYdLpXJx83SV/?mibextid=LQQJ4d"
+        className=" text-4xl"
+      >
+        <AiFillFacebook />
+      </a>
     </footer>
   );
 };
-//  https://www.facebook.com/share/sHBNDYdLpXJx83SV/?mibextid=LQQJ4d
+//
 export { Footer };
