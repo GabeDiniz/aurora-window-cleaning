d945468e08bcb137ea7a39870e665e27c1b663b2
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-05-12 13:32:39 -0400

    [Update] styling

--- src/components/sections/our-team/Hero.section.tsx
+++ src/components/sections/our-team/Hero.section.tsx
@@ -2,14 +2,14 @@ import { HeroImage } from "@assets";
 
 const OurTeamHero: React.FC = () => {
   return (
-    <section className="relative w-screen h-2/3 flex justify-start items-center">
+    <section className="relative w-screen h-2/3 flex justify-center items-center">
       <img
         className="absolute w-full h-full object-cover"
         src={HeroImage}
         alt="Hero"
       />
       <div className="absolute w-full h-full bg-black bg-opacity-70" />
-      <div className="z-10 flex flex-col gap-5 text-white px-10">
+      <div className="z-10 flex flex-col gap-5 text-white  w-full max-w-5xl">
         <h1 className="text-7xl uppercase animatecss animatecss-bounce">
           Meet our Team
         </h1>
