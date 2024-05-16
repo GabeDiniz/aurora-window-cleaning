c857ca125b6d70e253f9875f1811c9faac447ced
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-05-16 19:18:48 -0400

    [Update] Error404 page

--- src/pages/404Error.tsx
+++ src/pages/404Error.tsx
@@ -1,9 +1,17 @@
-
+import { Navbar } from "@components";
+import { Link } from "react-router-dom";
 
 const ErrorPage: React.FC = () => {
   return (
-    <div className="h-screen bg-laceWhite">
-    
+    <div className="">
+      <Navbar />
+      <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-slate-900">
+        <h1 className="text-5xl">OOPS!</h1>
+        <p>[Error 404] You're not supposed to be here... 🤔</p>
+        <Link className="mt-5" to="/">
+          <button className="border-white">Go home</button>
+        </Link>
+      </div>
     </div>
   );
 };
