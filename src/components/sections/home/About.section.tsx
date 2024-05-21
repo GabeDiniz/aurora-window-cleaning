004a08f3c7cfb8df460a273e7c1be38af9f3bd9a
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-05-20 22:31:15 -0400

    [Style] Make Homepage About responsive

--- src/components/sections/home/About.section.tsx
+++ src/components/sections/home/About.section.tsx
@@ -20,9 +20,9 @@ const AboutSection: React.FC = () => {
   return (
     <section className="py-20 flex flex-col justify-center items-center ]">
       <h1 className="text-4xl text-black">Why choose us</h1>
-      <div className="flex items-center mt-10 text-black max-w-7xl gap-10 px-20">
+      <div className="flex flex-col sm:flex-row items-center mt-10 text-black max-w-7xl gap-10 px-8 md:px-20">
         {chooseUs.map((content, i) => (
-          <div className="flex flex-col gap-5 text-center w-1/3" key={i}>
+          <div className="flex flex-col gap-5 text-center md:w-1/3" key={i}>
             <h2 className="text-xl">{content.title}</h2>
             <p>{content.content}</p>
           </div>
