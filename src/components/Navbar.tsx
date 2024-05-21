f3578d4f79781d8d2fb95795f45d003f6e852d78
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-05-21 19:06:58 -0400

    [Add] animation to NavBar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar: React.FC = () => {
 
   return (
     <section className="z-20 fixed px-10 py-5 w-full flex justify-between items-center text-lg text-white">
-      <Link to="/" className="">
+      <Link to="/" className="hover:scale-110 transition-all duration-100">
         StudentWorks
       </Link>
       <div className="z-10 md:hidden">
@@ -25,16 +25,28 @@ const Navbar: React.FC = () => {
           isOpen ? "flex animatecss-fadeIn" : "hidden"
         } flex-col md:flex-row fixed md:static top-0 right-0 pt-20 pl-16 pr-10 h-full md:h-auto text-right w-fit md:w-auto bg-slate-900 bg-opacity-95 md:bg-transparent p-5 md:p-0`}
       >
-        <Link to="/our-team" className="py-2 md:py-0">
+        <Link
+          to="/our-team"
+          className="py-2 md:py-0 hover:scale-110 transition-all duration-100"
+        >
           Our Team
         </Link>
-        <Link to="/services" className="py-2 md:py-0">
+        <Link
+          to="/services"
+          className="py-2 md:py-0 hover:scale-110 transition-all duration-100"
+        >
           Services
         </Link>
-        <Link to="/reviews" className="py-2 md:py-0">
+        <Link
+          to="/reviews"
+          className="py-2 md:py-0 hover:scale-110 transition-all duration-100"
+        >
           Reviews
         </Link>
-        <Link to="/free-estimate" className="py-2 md:py-0">
+        <Link
+          to="/free-estimate"
+          className="py-2 md:py-0 hover:scale-110 transition-all duration-100"
+        >
           Free Estimate
         </Link>
       </div>
