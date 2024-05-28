5590600e45418f2fc0c010ad4343922c5f8be00d
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-05-27 21:40:19 -0400

    [Update] Footer icons and func.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,25 @@
 import { AiFillFacebook } from "react-icons/ai";
+import { IoMdMail } from "react-icons/io";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="py-10 pl-10 flex flex-col gap-2 justify-center items-center bg-classicBlue text-laceWhite">
-      <h1 className="text-4xl uppercase">Get in touch</h1>
-      <p className="text-base ">efaro@hotmail.com</p>
-      <a
-        href="https://www.facebook.com/share/sHBNDYdLpXJx83SV/?mibextid=LQQJ4d"
-        target="_blank"
-        className="text-4xl hover:scale-125 transition-all"
-      >
-        <AiFillFacebook />
-      </a>
+    <footer className="text-4xl py-10 pl-10 flex flex-col gap-2 justify-center items-center bg-classicBlue text-laceWhite">
+      <h1 className="uppercase">Get in touch</h1>
+      <div className="flex gap-3">
+        <a
+          href="mailto:efaro@hotmail.com"
+          className="hover:scale-125 transition-all"
+        >
+          <IoMdMail />
+        </a>
+        <a
+          href="https://www.facebook.com/share/sHBNDYdLpXJx83SV/?mibextid=LQQJ4d"
+          target="_blank"
+          className="hover:scale-125 transition-all"
+        >
+          <AiFillFacebook />
+        </a>
+      </div>
     </footer>
   );
 };
