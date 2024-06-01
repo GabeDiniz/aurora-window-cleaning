96090c7c678fed8221b0cda87f2679577786f606
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-06-01 13:19:27 -0400

    [Responsive] FreeEstimate Header

--- src/components/sections/estimate-form/Header.section.tsx
+++ src/components/sections/estimate-form/Header.section.tsx
@@ -2,18 +2,18 @@ import { HeroImage } from "@assets";
 
 const EstimateHeader: React.FC = () => {
   return (
-    <section className="relative h-2/3 flex justify-center items-center">
+    <section className="relative h-2/3 flex justify-center">
       <img
         className="absolute w-full h-full object-cover"
         src={HeroImage}
         alt="Hero"
       />
       <div className="absolute w-full h-full bg-black bg-opacity-70" />
-      <div className="z-10 text-white max-w-5xl flex flex-col gap-3 text-center items-center">
-        <h1 className="text-7xl uppercase animatecss animatecss-bounce">
+      <div className="z-10 text-white max-w-5xl flex flex-col gap-3 text-center py-20 md:py-0 px-10 md:px-0 md:justify-center">
+        <h1 className="text-5xl md:text-7xl  uppercase animatecss animatecss-bounce">
           Book your free estimate!
         </h1>
-        <p className="text-xl">
+        <p className="text-sm md:text-xl">
           If you have any questions or inquiries feel free to call or email for
           more information.
         </p>
