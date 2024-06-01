ff805eb0c36f4860fc051013426aaf00a9a6da62
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-05-31 22:54:16 -0400

    [Fix] missing indexing

--- src/components/sections/services/Services.section.tsx
+++ src/components/sections/services/Services.section.tsx
@@ -6,15 +6,21 @@ const Services: React.FC = () => {
     <section className="relative bg-laceWhite">
       <div className="py-10">
         {/* MAP ALL SERVICES */}
-        {servicesData.map((product) => {
+        {servicesData.map((product, index) => {
           return (
-            <div className="text-3xl flex flex-col gap-5 mb-10 justify-center items-center">
+            <div
+              className="text-3xl flex flex-col gap-5 mb-10 justify-center items-center"
+              key={index}
+            >
               <h1>{product.title}</h1>
               <div className="flex gap-5 mx-10 items-start">
                 {/* MAP EACH SERVICE SECTION DATA */}
-                {product.data.map((item) => {
+                {product.data.map((item, index) => {
                   return (
-                    <div className="flex flex-col justify-center gap-3">
+                    <div
+                      className="flex flex-col justify-center gap-3"
+                      key={index}
+                    >
                       <img src={item.banner} className="w-full rounded-md" />
                       <h2 className="text-base font-bold">{item.subtitle}</h2>
                       <p className="text-sm font-thin">{item.subtext}</p>
