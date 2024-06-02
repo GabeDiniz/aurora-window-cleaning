4d45e6a5bf838bfcc71eb2384a9cf031c1264df8
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-06-02 18:55:17 -0400

    [Responsive] Team Hero section

--- src/components/sections/our-team/Hero.section.tsx
+++ src/components/sections/our-team/Hero.section.tsx
@@ -9,11 +9,11 @@ const OurTeamHero: React.FC = () => {
         alt="Hero"
       />
       <div className="absolute w-full h-full bg-black bg-opacity-70" />
-      <div className="z-10 flex flex-col gap-5 text-white w-full max-w-5xl pl-12">
-        <h1 className="text-7xl uppercase animatecss animatecss-bounce">
+      <div className="z-10 flex flex-col gap-5 text-white w-full max-w-5xl px-6 sm:px-10 md:pl-12">
+        <h1 className="text-5xl sm:text-7xl uppercase animatecss animatecss-bounce">
           Meet our Team
         </h1>
-        <p className="max-w-2xl pl-2">
+        <p className="text-sm sm:text-xl max-w-2xl pl-2">
           Welcome to Aurora Student Window Cleaning! We are a hardworking and
           dedicated team of students operating in Aurora. Your satisfaction is
           our number one priority. From start to finish we want to ensure that
