fbfae6358b8e2860047325c96b43474ee733e9e1
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-06-07 16:07:06 -0400

    [Update] styling for review cards

--- src/components/sections/reviews/ReviewCards.tsx
+++ src/components/sections/reviews/ReviewCards.tsx
@@ -3,12 +3,12 @@ import { reviewsData } from "@data";
 const ReviewCards: React.FC = () => {
   return (
     <section className="relative bg-laceWhite flex justify-center">
-      <div className="flex flex-wrap justify-center gap-5 px-10 py-10 max-w-7xl">
+      <div className="flex flex-wrap justify-center gap-5 px-10 py-10 max-w-7xl text-white">
         {/* MAP EACH CARD */}
         {reviewsData.map((review, index) => {
           return (
             <div
-              className="bg-gray-300 p-5 rounded-xl w-80 flex flex-col gap-4 drop-shadow-lg"
+              className="bg-classicBlue p-5 rounded-xl w-80 flex flex-col gap-4 drop-shadow-lg"
               key={index}
             >
               <div className="flex items-center gap-5">
