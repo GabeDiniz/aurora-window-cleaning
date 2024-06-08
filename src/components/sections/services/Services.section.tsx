d3fed53915e4ef01d664a9fc02e010430ab73833
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-06-08 14:29:52 -0400

    [Update] services styling

--- src/components/sections/services/Services.section.tsx
+++ src/components/sections/services/Services.section.tsx
@@ -12,7 +12,7 @@ const Services: React.FC = () => {
               className="text-3xl flex flex-col gap-5 mb-10 justify-center items-center"
               key={index}
             >
-              <h1>{product.title}</h1>
+              <h1 className="font-bold">{product.title}</h1>
               <div className="flex gap-5 mx-10 items-start">
                 {/* MAP EACH SERVICE SECTION DATA */}
                 {product.data.map((item, index) => {
@@ -25,7 +25,7 @@ const Services: React.FC = () => {
                         src={item.banner}
                         className="w-full rounded-md drop-shadow-md"
                       />
-                      <h2 className="text-base font-bold">{item.subtitle}</h2>
+                      <h2 className="text-base ">{item.subtitle}</h2>
                       <p className="text-sm font-thin">{item.subtext}</p>
                     </div>
                   );
