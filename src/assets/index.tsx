629402282e3dd2fd492402f35f28d66e20bf3bfc
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-06-25 22:16:25 -0400

    [update] index images

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,10 +1,38 @@
 /*
  *
- * Hero Section
+ * Hero
  *
  */
 import HeroImage from "./hero/hero-home.jpg";
 import Eric1 from "./hero/image.jpg";
 
-export { HeroImage };
-export { Eric1 };
+export { HeroImage, Eric1 };
+
+/*
+ *
+ * Services
+ *
+ */
+import ExtEaves from "./services/ext-eaves.jpeg";
+import GutterGuard from "./services/gutter-guard.png";
+import IntEaves from "./services/int-eaves.jpg";
+import IntWindowCleaningBetter from "./services/int-window-cleaning-Better.png";
+import IntWindowCleaning from "./services/int-window-cleaning.png";
+import windowCleaning from "./services/window-cleaning.jpeg";
+import PolySanding from "./services/poly-sanding.png";
+import PressureWashing from "./services/pressure-washing.png";
+import RainX from "./services/rainx-application.jpg";
+import SealingBeforeAfter from "./services/sealing-before-after.webp";
+
+export {
+  ExtEaves,
+  GutterGuard,
+  IntEaves,
+  IntWindowCleaningBetter,
+  IntWindowCleaning,
+  windowCleaning,
+  PolySanding,
+  PressureWashing,
+  RainX,
+  SealingBeforeAfter,
+};
